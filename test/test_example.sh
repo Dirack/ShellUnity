97b7b3855befdd135b4626be9eba4ba8f38386eb
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2021-03-30 12:23:33 -0300

    Cabeçalho de documentação do teste test_example.sh
    Este é um teste das assertions básicas da shellUnity

--- test/test_example.sh
+++ test/test_example.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+# test_example.sh (Shell Script)
+# 
+# Objetivo: Exemplos de testes unitários básicos da shellUnity.
+# 
+# Site: https://dirack.github.io
+# 
+# Versão 1.0
+# 
+# Programador: Rodolfo A C Neves (Dirack) 30/10/2020
+# 
+# Email: rodolfo_profissional@hotmail.com
+# 
+# Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
 
 source $SHELLUNITY_PATH/shellUnity
 
