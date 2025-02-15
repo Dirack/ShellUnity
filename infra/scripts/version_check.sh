9402095679b10b6af8f5a702f06ff03221411cb2
Dirack <rodolfo_profissional@hotmail.com>
2025-02-15 13:47:06 -0300

    [chore](version check): Add a version check script

--- infra/scripts/version_check.sh
+++ infra/scripts/version_check.sh
@@ -2,12 +2,20 @@
 #
 # Check current version based on active develop branch name
 
-CURRENTVERSION="3.0.0"
+CURRENTVERSION=$(git tag | sort -r | tr '\n' ' ' | cut -d" " -f1)
+CURRENTVERSION=${CURRENTVERSION##*v}
+
+echo "CURRENT VERSION: ${CURRENTVERSION}"
+
+if [ -z "$CURRENTVERSION" ]
+then
+	exit 1
+fi
 
 VERSIONDOC=$(cat docs/VERSION.md)
 
 if [ "$VERSIONDOC" == "$CURRENTVERSION" ]; then
-	echo "Version checked: $VERSIONDOC"
+	echo "Version checked (VERSION.md file): $VERSIONDOC"
 	exit 0
 else
 	echo "Package version in VERSION.md file does not seem correct!"
